ce482e93dc0810b1a5aa4c01c8ace127517b726c
Horacio Ibrahim <horacioibrahim@gmail.com>
2015-02-22 12:37:57 -0300

    Fix version

--- lib/iugu/version.py
+++ lib/iugu/version.py
@@ -1,8 +1,8 @@
-__version__ = "0.9.5-1"
+__version__ = "0.9.5-2"
 
 #------------------------------------------------------------------------------
 # CHANGELOG:
 # ... oldest changes
 # 2014-07-07 v0.9.3 - Removed requirement of config.py variables (token, account)
 # 2015-02-22 v0.9.4 - Supported changes from API
-# 2015-02-22 v0.9.5-1 - Supported changes from API
+# 2015-02-22 v0.9.5-1+ - Supported changes from API
